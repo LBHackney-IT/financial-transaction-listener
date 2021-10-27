e4dafcc10e8ef6c3d36a41dd07f5277d62cb2d06
eddygitgub <87383463+EdwardAnoghena@users.noreply.github.com>
2021-10-27 18:07:01 +0100

    updated serverless.yml and transactionApiGateway for token auth

--- FinancialTransactionListener/Gateway/TransactionApiGateway.cs
+++ FinancialTransactionListener/Gateway/TransactionApiGateway.cs
@@ -46,8 +46,9 @@ namespace FinancialTransactionListener.Gateway
             var client = _httpClientFactory.CreateClient();
             var getTransactionApiRoute = $"{_getTransactionApiRoute}/transactions/{id}";
 
-            //client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(_getTransactionApiToken);
-            client.DefaultRequestHeaders.Add("x-api-key", _getTransactionApiToken);
+            client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(_getTransactionApiToken);
+            //_httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            //client.DefaultRequestHeaders.Add("x-api-key", _getTransactionApiToken);
             var response = await RetryService.DoAsync(client.GetAsync(new Uri(getTransactionApiRoute)), 2, TimeSpan.FromSeconds(2)).ConfigureAwait(false);
 
             if (response.StatusCode is HttpStatusCode.NotFound)
